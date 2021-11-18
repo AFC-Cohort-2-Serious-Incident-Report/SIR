7cbe0739b701655bda4a0619f29d05d4b7489e74
Joseph Myrick <joseph.l.myrick2.mil@swf.army.mil>
2021-11-18 16:04:19 -0600

    Empty shell app.

---
+++ src/test/java/com/cohort2/seriousincidentreport/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cohort2.seriousincidentreport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

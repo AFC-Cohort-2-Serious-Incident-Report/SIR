fbc3ae75353020786458b3c467cef7772e17cd2a
factoryuser1 <factoryuser1@yahoo.com>
2021-11-29 11:42:51 -0600

    created Responder test and json file

--- sir-client/src/ResponderView/ResponderView.test.tsx
+++ sir-client/src/ResponderView/ResponderView.test.tsx
@@ -1,5 +1,16 @@
 import { render, screen } from '@testing-library/react';
+import { setupServer } from 'msw/node';
+import { rest } from 'msw';
 import ResponderView from './ResponderView';
+import data from '../Responder_Test_Data_50.json';
+
+const server = setupServer(
+  rest.get('/api/incidents', (req, res, ctx) => res(ctx.json(data))),
+);
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
 
 describe('ResponderView', () => {
   beforeEach(() => {
@@ -7,7 +18,23 @@ describe('ResponderView', () => {
   });
 
   it('should render components correctly', () => {
-    expect(screen.getByRole('heading', { name: /incident reports/i })).toBeInTheDocument();
-    expect(screen.getByRole('heading', { name: /reports/i })).toBeInTheDocument();
+    expect(screen.getByRole('heading', { name: /^incident reports/i })).toBeInTheDocument();
+    expect(screen.getByRole('heading', { name: /^reports/i })).toBeInTheDocument();
+  });
+
+  it('should render table headers', () => {
+    expect(screen.getByRole('checkbox', { checked: false }));
+    expect(screen.getByText(/^Event Date/i)).toBeInTheDocument();
+    expect(screen.getByText(/^Location/i)).toBeInTheDocument();
+    // expect(screen.getByText(/^Incident Type/i)).toBeInTheDocument();
+    expect(screen.getByText(/^Harm/i)).toBeInTheDocument();
+    // expect(screen.getByText('Individual(s) Involved')).toBeInTheDocument();
+    expect(screen.getByText(/^Event Type/i)).toBeInTheDocument();
+    expect(screen.getByText(/^details/i)).toBeInTheDocument();
+  });
+  it('should render json for responder', () => {
+    expect(screen.getByText('03/27/2021')).toBeInTheDocument();
+    expect(screen.getByText('03/27/2021')).toBeInTheDocument();
+    expect(screen.getByText('visa')).toBeInTheDocument();
   });
 });
--- sir-client/src/ResponderView/ResponderView.tsx
+++ sir-client/src/ResponderView/ResponderView.tsx
@@ -1,5 +1,20 @@
 const ResponderView = () => (
-  <h1>Incident Reports</h1>
+  <div>
+    <h1>Incident Reports</h1>
+    <h3>Reports</h3>
+    <div>
+      <div><input type="checkbox" /></div>
+      <div>Event Date</div>
+      <div>Location</div>
+      {/* <div>Incident Type</div> */}
+      <div>Harm</div>
+      {/* <div>Individual(s) Involved</div> */}
+      <div>Event Type</div>
+      <div>Details</div>
+    </div>
+
+  </div>
+
 );
 
 export default ResponderView;

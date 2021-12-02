da46b8b97e7fed1f3c9907b0957d6096da18e5f7
uplusion23 <uplusion23@gmail.com>
2021-12-02 15:35:22 -0700

    Finish pagination component. Add proper styles.

--- sir-client/src/Components/Pagination.tsx
+++ sir-client/src/Components/Pagination.tsx
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import Dropdown from 'react-dropdown';
+import { set } from 'msw/lib/types/context';
 
 // eslint-disable-next-line @typescript-eslint/ban-types
 type paginationProps = {
@@ -21,40 +23,35 @@ const Pagination: React.FC<paginationProps> = ({
   currentPage,
   offset,
   navigatePage,
-}: paginationProps) => {
-  for (let x = 0; x < 1; x = +1) console.log('');
-  console.log(
-    pages,
-    size,
-    firstPage,
-    lastPage,
-    totalCount,
-    currentPage,
-    offset,
-    navigatePage,
-  );
-  return (
-    <div className="pagination">
-      <div className="size-container">
-        {`Rows per page: ${size}`}
-        <button type="button">
-          <i className="gg-chevron-down" />
-        </button>
-      </div>
-      <span>
-        {`${offset + 1}-${offset + size} of ${totalCount}`}
-      </span>
-      <div className="page-controls">
-        <button type="button" disabled={firstPage} onClick={() => navigatePage(currentPage - 1, size)}>
-          <i className="gg-chevron-left" />
-        </button>
-        <button type="button" disabled={lastPage} onClick={() => navigatePage(currentPage + 1, size)}>
-          <i className="gg-chevron-right" />
-        </button>
-      </div>
+}: paginationProps) => (
+  <div
+    className="pagination"
+    data-testid="pagination"
+  >
+    <div className="size-container">
+      Rows per page:
+      <Dropdown
+        options={[
+          '5', '10', '15',
+        ]}
+        onChange={(val) => navigatePage(0, parseInt(val.value, 10))}
+        value="5"
+        placeholder="5"
+      />
     </div>
-  );
-};
+    <span>
+      {`${offset + 1}-${offset + size} of ${totalCount}`}
+    </span>
+    <div className="page-controls">
+      <button type="button" disabled={firstPage} onClick={() => navigatePage(currentPage - 1, size)}>
+        <i className="gg-chevron-left" />
+      </button>
+      <button type="button" disabled={lastPage} onClick={() => navigatePage(currentPage + 1, size)}>
+        <i className="gg-chevron-right" />
+      </button>
+    </div>
+  </div>
+);
 
 export default Pagination;
 

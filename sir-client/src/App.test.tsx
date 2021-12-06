dce9e382cc5ba814b3490aa60cf9e5eb09901602
Jonathon Phillips <jonathan.phillips@austincc.edu>
2021-12-06 10:27:26 -0600

    Refactored reporter routes to use ReporterView.

--- sir-client/src/App.test.tsx
+++ sir-client/src/App.test.tsx
@@ -8,29 +8,27 @@ test('renders reporter and responder links with reporter focused', async () => {
 
   // render App
   render(<App />);
-  const reporterLink = screen.getByRole('link', { name: /Reporter/i });
-  const responderLink = screen.getByRole('link', { name: /Responder/i });
   // assert App loads with default nav elements
-  expect(reporterLink).toBeInTheDocument();
-  expect(responderLink).toBeInTheDocument();
+  expect(screen.getByRole('link', { name: /Reporter/i })).toBeInTheDocument();
+  expect(screen.getByRole('link', { name: /Responder/i })).toBeInTheDocument();
   expect(screen.getByTestId('swfLogo')).toBeInTheDocument();
-  // assert reporterLink is focused
-  expect(reporterLink).toHaveClass('focused');
-  expect(responderLink).toHaveClass('unfocused');
+  // assert screen.getByRole('link', { name: /Reporter/i }) is focused
+  expect(screen.getByRole('link', { name: /Reporter/i })).toHaveClass('focused');
+  expect(screen.getByRole('link', { name: /Responder/i })).toHaveClass('unfocused');
   expect(screen.getByRole('heading', { name: /Incident Report Form/i })).toBeInTheDocument();
   expect(screen.queryByText(/Incident Reports/i)).not.toBeInTheDocument();
-  // click responderLink
-  await userEvent.click(responderLink);
-  // assert responderLink is focused
-  expect(reporterLink).toHaveClass('unfocused');
-  expect(responderLink).toHaveClass('focused');
+  // click screen.getByRole('link', { name: /Responder/i })
+  await userEvent.click(screen.getByRole('link', { name: /Responder/i }));
+  // assert screen.getByRole('link', { name: /Responder/i }) is focused
+  expect(screen.getByRole('link', { name: /Reporter/i })).toHaveClass('unfocused');
+  expect(screen.getByRole('link', { name: /Responder/i })).toHaveClass('focused');
   expect(screen.queryByText(/Incident Report Form/i)).not.toBeInTheDocument();
   expect(screen.getByRole('heading', { name: /Incident Reports/i })).toBeInTheDocument();
-  // click reporterLink
-  await userEvent.click(reporterLink);
-  // assert reporterLink is focused
-  expect(reporterLink).toHaveClass('focused');
-  expect(responderLink).toHaveClass('unfocused');
+  // click screen.getByRole('link', { name: /Reporter/i })
+  await userEvent.click(screen.getByRole('link', { name: /Reporter/i }));
+  // assert screen.getByRole('link', { name: /Reporter/i }) is focused
+  expect(screen.getByRole('link', { name: /Reporter/i })).toHaveClass('focused');
+  expect(screen.getByRole('link', { name: /Responder/i })).toHaveClass('unfocused');
   expect(screen.getByRole('heading', { name: /Incident Report Form/i })).toBeInTheDocument();
   expect(screen.queryByText(/Incident Reports/i)).not.toBeInTheDocument();
 });
---
+++ sir-client/src/Views/ReporterView.tsx
@@ -0,0 +1,12 @@
+import React, { FC, ReactElement } from 'react';
+import NavBar from '../NavBar/NavBar';
+import SirForm from '../SirForm/SirForm';
+
+const ReporterView: FC = ():ReactElement => (
+  <div>
+    <header><NavBar isResponder={false} /></header>
+    <SirForm />
+  </div>
+);
+
+export default ReporterView;

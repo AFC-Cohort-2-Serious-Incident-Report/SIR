75da87298b8bac27dcfdce719cfe81a99d51731c
uplusion23 <uplusion23@gmail.com>
2021-12-08 14:14:49 -0600

    Backend Search

--- sir-server/src/main/java/com/cohort2/seriousincidentreport/Incident/IncidentController.java
+++ sir-server/src/main/java/com/cohort2/seriousincidentreport/Incident/IncidentController.java
@@ -2,14 +2,11 @@ package com.cohort2.seriousincidentreport.Incident;
 
 import com.cohort2.seriousincidentreport.Individuals.IndividualsRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import java.util.ArrayList;
 
 @RestController
@@ -28,20 +25,23 @@ public class IncidentController {
         return this.repository.save(incident);
     }
 
-//    @GetMapping
-//    public Iterable<Incident> getAllIncidents(@RequestParam(defaultValue = "5") int size, @RequestParam(defaultValue = "0") int page) {
-//        final var pagedResults = this.repository.findAll(PageRequest.of(page, size));
-//        if (pagedResults.hasContent())
-//            return pagedResults.getContent();
-//        else
-//            return new ArrayList<Incident>();
-//    }
-
     @GetMapping
     public ResponseEntity<Iterable<Incident>> getIncidents(Pageable pageable) {
         Iterable<Incident> incidents = new ArrayList<Incident>();
         try {
             incidents = this.repository.findAll(pageable);
+        } catch (Exception e) {
+            return new ResponseEntity<Iterable<Incident>>(incidents, null, 503);
+        } finally {
+            return new ResponseEntity<Iterable<Incident>>(incidents, null, 200);
+        }
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<Iterable<Incident>> getIncidentsSearch(@RequestParam(defaultValue = "") String query, Pageable pageable) {
+        Iterable<Incident> incidents = new ArrayList<Incident>();
+        try {
+            incidents = this.repository.findAllByPatientInfo_PatientNameContaining(query, pageable);
         } catch (Exception e) {
             return new ResponseEntity<Iterable<Incident>>(incidents, null, 503);
         } finally {

5d038d96a6a525fa97f1f84da1589ecdf2f080e1
Kyle McCain <kyle.t.mccain.mil@swf.army.mil>
2022-01-03 09:06:38 -0600

    Removed comments

--- sir-server/src/test/java/com/cohort2/seriousincidentreport/ApplicationTests.java
+++ sir-server/src/test/java/com/cohort2/seriousincidentreport/ApplicationTests.java
@@ -2,18 +2,10 @@ package com.cohort2.seriousincidentreport;
 
 import com.cohort2.seriousincidentreport.Incident.IncidentRepository;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-
-import static org.hamcrest.core.Is.is;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 
 
 @SpringBootTest
@@ -26,33 +18,7 @@ class ApplicationTests {
     @Autowired
     IncidentRepository repository;
 
-    @BeforeEach
-    public void setupTest() {
-//		(ObjectType) (ObjectName) = new (ObjectType) ()
-//		(ObjectName.setter)
-//				(this) (newObjectRepository) (method) ((newItem))
-	}
-
-        @Test
-        void contextLoads () {
-        }
-//
-//        @Transactional
-//        @Rollback
-//        @Test
-//        public void sampleTest () THROWS Exception {
-//            //Setup
-//            Create New Item.
-//                    Set New Item Properties.
-//                    This.repository.save
-//            Set id variable if needed. ( long =getid)
-////Execution
-//            this.mvc.perform(get / post / patch / delete("/url"))
-//
-////Assertion
-//                    .andExpect(status().isOk())
-//                    .andExpect(jsonPath("$[5].description", is("New Test Item!!")));
-//        }
-
-
+    @Test
+    void contextLoads() {
     }
+}
